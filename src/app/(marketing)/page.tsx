85417f72c2a74270302558a36d8ad1c47401eb68
Jecé Xavier <jecexavier12@gmail.com>
2025-12-06 20:46:55 -0300

    feat(home): integrate new sections and remove SocialProof

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,17 +1,27 @@
 import { Hero } from './components/home/Hero'
-import { FeaturedServices } from './components/home/FeaturedServices'
+import { AboutSummary } from './components/home/AboutSummary'
 import { ImpactMetrics } from './components/home/ImpactMetrics'
-import { SocialProof } from './components/home/SocialProof'
+import { FeaturedServices } from './components/home/FeaturedServices'
+import { Methodology } from './components/home/Methodology'
+import { Clients } from './components/home/Clients'
+import { FAQ } from './components/home/FAQ'
+import { BlogPreview } from './components/home/BlogPreview'
+import { LeadCapture } from './components/home/LeadCapture'
 
 export default function MarketingHome() {
   return (
     <div className="flex flex-col min-h-screen bg-white-essential">
       <Hero />
-      <FeaturedServices />
-      <div className="bg-green-900">
+      <AboutSummary />
+      <div className="bg-horizon-green">
         <ImpactMetrics />
       </div>
-      <SocialProof />
+      <FeaturedServices />
+      <Methodology />
+      <Clients />
+      <FAQ />
+      <BlogPreview />
+      <LeadCapture />
     </div>
   )
 }

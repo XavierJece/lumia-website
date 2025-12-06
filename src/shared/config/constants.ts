24b9865ec1d3f6d09c74d1a1258c2bf3c4cb28b3
Jecé Xavier <jecexavier12@gmail.com>
2025-12-06 20:46:06 -0300

    feat(config): add constants and update Next.js image configuration

---
+++ src/shared/config/constants.ts
@@ -0,0 +1,8 @@
+export const CONTACT_INFO = {
+  whatsapp: {
+    number: '5511947305880',
+    display: '(11) 94730-5880',
+    link: 'https://wa.me/5511947305880',
+  },
+  email: 'contato@lumia.com.br',
+}
